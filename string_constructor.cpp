b266cebedfa57791d573f6ef186d92ac04118e41
Masrafi404 <mdmasrafi902@gmail.com>
2025-06-03 06:57:49 +0600

    string constructor

---
+++ string_constructor.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // string s = "Hello World";
+
+    // way 1
+    // string s("Hello World");
+
+    // way 2
+    // string s("Hello  World", 5);
+
+    // way 3
+    // string s("Hello World");
+    // string t(s, 6);
+
+    string s(5, 'A');
+
+    cout << s;
+
+    return 0;
+}
